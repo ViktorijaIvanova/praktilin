072fa74fcf89d962239cf2addf003951e3d36461
opilane <opilane@HP-CZC6297BB7>
2022-12-14 15:41:24 +0200

    Add project files.

---
+++ praktilinetöözadachi.py
@@ -0,0 +1,23 @@
+#2 osa
+from math import *
+n = int(input("sisestage number: "))
+v = int(input("sisestage number: "))
+b = int(input("sisestage number: "))
+if("positiivne number"):
+    if n>0 and v>0 and b>0:
+        if n + v + b == 180
+           if n == v and v==b:
+               print("võrdhaarne kolmnurk")
+           elif n == v or n == b or v == b
+
+
+
+
+
+#1 osa
+from math import *
+n = int(input("sisestage number: "))
+if(n > 0):
+    print("positiivne number")
+else:
+    print("negativne number")
